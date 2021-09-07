2cbf7ac42e37cd9c228b83365b09a7e7f2c8bcdb
snatadas <91snatasurobhita.com>
2021-09-07 08:07:56 +0530

    Testing push

--- src/CssSelector.java
+++ src/CssSelector.java
@@ -4,6 +4,7 @@ public class CssSelector {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\snata\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
+		System.out.print("To test");
 
 	}
 
