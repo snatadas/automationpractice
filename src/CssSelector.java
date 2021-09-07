7f947489f86b3c2f4e3918fcb5c5007db907360e
snatadas <91snatasurobhita.com>
2021-09-07 08:15:33 +0530

    undoing prev changes

--- src/CssSelector.java
+++ src/CssSelector.java
@@ -4,7 +4,6 @@ public class CssSelector {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\snata\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
-		System.out.print("To test");
 
 	}
 
